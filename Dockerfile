1ac1dd8692335b63cfcff11513e53b7ff57a8851
YeHam Kim <yehamkim@YeHams-MacBook-Air.local>
2023-01-18 14:54:03 +0900

    feat: certbot-nginx 파트 추가

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
 FROM nginx:1.21.6
 
+RUN apt-get update \
+    && apt-get install -y openssl \
+	&& apt-get install -y certbot \
+	&& apt-get install -y python3-certbot-nginx
+
 COPY ./nginx.conf /etc/nginx/nginx.conf
-COPY ./init.sh /init.sh
-COPY ./html /usr/share/nginx/html
 
 CMD ["/bin/bash", "init.sh"]
--- init.sh
+++ init.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+mkdir -p /etc/letsencrypt/live/$NGINX_HOST
+mkdir -p /var/www/certbot
+openssl req -x509 -nodes -newkey rsa:4096 -days 1\
+    -keyout "/etc/letsencrypt/live/$NGINX_HOST/privkey.pem" \
+    -out "/etc/letsencrypt/live/$NGINX_HOST/fullchain.pem" \
+    -subj "/CN=localhost"
+
 bash /docker-entrypoint.d/20-envsubst-on-templates.sh
 nginx -g "daemon off;"
