020bec2688a0d247e0905c1f6bad16ddf80cad46
yamkant <dev.yamkim@gmail.com>
2023-05-01 16:04:32 +0900

    feat: [Setting: Deployment] Django 배포환경 설정을 위한 setting 파일 분리 및 /profile 추가

--- django-server/django/code/api/controllers/HealthCheckController.py
+++ django-server/django/code/api/controllers/HealthCheckController.py
@@ -1,17 +1,7 @@
-from rest_framework import viewsets
-from rest_framework.decorators import action
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import permissions
 from rest_framework import status
 
-class HealthCheckController(viewsets.ModelViewSet): 
-    # queryset = User.objects.all()
-    # serializer_class = UserSerializer
-    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-    def list(self, req):
+class HealthCheckController(APIView): 
+    def get(self, req, format=None):
         return Response({"status": "ok"}, status=status.HTTP_200_OK)
-
-health_check_list = HealthCheckController.as_view({
-    'get': 'list',
-})
--- django-server/django/code/api/urls.py
+++ django-server/django/code/api/urls.py
@@ -2,10 +2,12 @@ from django.urls import include, path
 from rest_framework.routers import Route, DynamicRoute, SimpleRouter, DefaultRouter
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from api.controllers import HealthCheckController
+from api.controllers.HealthCheckController import HealthCheckController
+from api.controllers.ProfileController import ProfileController
 
 app_name = 'users'
 
 urlpatterns = format_suffix_patterns([
-    path('', HealthCheckController.health_check_list, name='health_check_list'),
+    path('health_check/', HealthCheckController.as_view(), name='healthcheck_controller'),
+    path('profile/', ProfileController.as_view(), name='healthcheck_controller'),
 ])
