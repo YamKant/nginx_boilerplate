b36a81aca20958fe3980d5c3462df1a506ea8d89
yamkant <dev.yamkim@gmail.com>
2023-05-02 21:23:52 +0900

    feat: [Setting: Deployment] 두개의 서버로 도커서버 구성

--- django-server/django/code/api/controllers/ProfileController.py
+++ django-server/django/code/api/controllers/ProfileController.py
@@ -1,10 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
 import os
 
 class ProfileController(APIView): 
     def get(self, req, format=None):
-        settingProfile = os.environ.get('DJANGO_SETTINGS_MODULE').split('.')[-1]
+        settingProfile = os.environ.get('PROFILE')
         return Response(settingProfile, status=status.HTTP_200_OK)
