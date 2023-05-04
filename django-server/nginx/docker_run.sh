ff349e7820849dd58c77e96962894d3f0beab2bf
yamkant <dev.yamkim@gmail.com>
2023-05-05 00:08:19 +0900

    feat: [Setting: Server] nginx 단일 도커 구축

--- django-server/nginx/docker_run.sh
+++ django-server/nginx/docker_run.sh
@@ -2,6 +2,7 @@
 
 CUR_PATH="$( cd "$( dirname "$0" )" && pwd -P )"
 IMAGE_NAME=my-nginx:1.0
+MOUNT_DIR=$CUR_PATH/code/logs
 
 docker build -t $IMAGE_NAME $CUR_PATH
-docker run -d -p 80:80 --name my-nginx $IMAGE_NAME
+docker run -d -p 80:80 -p 443:443 -v $MOUNT_DIR:/var/log/nginx/logs --name my-nginx $IMAGE_NAME
